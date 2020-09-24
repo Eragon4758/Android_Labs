0c1aab899a81d55133644c7a57ff3ab116188a88
Eragon4758 <cayle@rogers.com>
2020-09-24 14:52:54 -0400

    added 3 layouts for lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.*;
 
 import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_gridlayout);
+        final Button clickBtn = findViewById(R.id.button1);
+        clickBtn.setOnClickListener((click) -> {
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message),Toast.LENGTH_LONG).show();});
+        final CheckBox chBox = findViewById(R.id.CheckBox);
+        chBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean b) {
+                if(b) {
+                    Snackbar.make(chBox, getResources().getString(R.string.check_message) + " " +getResources().getString(R.string.on_string), Snackbar.LENGTH_LONG)
+                    .setAction(getResources().getString(R.string.undo_string), click -> cb.setChecked(!b)).show();
+                }
+                else{
+                    Snackbar.make(chBox, getResources().getString(R.string.check_message) + " " + getResources().getString(R.string.off_string), Snackbar.LENGTH_LONG)
+                            .setAction(getResources().getString(R.string.undo_string), click -> cb.setChecked(!b)).show();
+                }
+            }
+        });
+        final Switch sw = findViewById(R.id.Switch);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean b) {
+                if(b) {
+                    Snackbar.make(sw, getResources().getString(R.string.switch_message) + " " + getResources().getString(R.string.on_string), Snackbar.LENGTH_LONG)
+                            .setAction(getResources().getString(R.string.undo_string), click -> sw.setChecked(!b)).show();
+                }
+                else{
+                    Snackbar.make(sw, getResources().getString(R.string.switch_message) + " " + getResources().getString(R.string.off_string), Snackbar.LENGTH_LONG)
+                            .setAction(getResources().getString(R.string.undo_string), click -> sw.setChecked(!b)).show();
+                }
+            }
+        });
     }
 }
